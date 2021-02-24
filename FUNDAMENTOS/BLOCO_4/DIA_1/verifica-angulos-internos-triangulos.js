0f75cd016206461268182de326d91cac75446563
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-02-24 19:12:31 -0300

    Adicionando o programa que verifica a soma dos angulos internos dos triangulos com correçoes nos nomes das variaveis

--- FUNDAMENTOS/BLOCO_4/DIA_1/verifica-angulos-internos-triangulos.js
+++ FUNDAMENTOS/BLOCO_4/DIA_1/verifica-angulos-internos-triangulos.js
@@ -1,10 +1,10 @@
-let anguloVerticeA = 20;
-let anguloVerticeB = 90;
-let anguloVerticeC = 60;
-let somaAngulosInternos = anguloVerticeA+anguloVerticeB+anguloVerticeC;
+let anguloInternoVerticeA = 20;
+let anguloInternoVerticeB = 90;
+let anguloInternoVerticeC = 60;
+let somaAngulosInternos = anguloInternoVerticeA+anguloInternoVerticeB+anguloInternoVerticeC;
 
 if (somaAngulosInternos === 180) {
-  console.log('Os ângulos dos triângulos estão corretos!');
+  console.log('Os ângulos internos dos triângulos estão corretos!');
 } else {
-  console.log('Os ângulos dos triângulos estão incorretos!');
+  console.log('Os ângulos internos dos triângulos estão incorretos!');
 }
