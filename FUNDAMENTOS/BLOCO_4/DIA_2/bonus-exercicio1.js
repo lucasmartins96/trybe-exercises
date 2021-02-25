08e6574d78a1ce27110e4de6fcb0fb82902c1c3d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-02-25 15:56:20 -0300

    Adicionando exercicio bonus 1

---
+++ FUNDAMENTOS/BLOCO_4/DIA_2/bonus-exercicio1.js
@@ -0,0 +1,16 @@
+//Exercício Bônus 1: Ordene o array numbers em ordem crescente e imprima seus valores;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers);
+
+for (let i = 1; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] < numbers[j]) {
+      let aux = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = aux;
+    }
+  }
+}
+
+console.log(numbers);
