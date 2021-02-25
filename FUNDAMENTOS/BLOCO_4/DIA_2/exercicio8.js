1dc7d19ef06c77f3f9f9bf04df39edbff4faf25b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-02-25 14:53:01 -0300

    Adicionando a resoluçao do exercicio 8

---
+++ FUNDAMENTOS/BLOCO_4/DIA_2/exercicio8.js
@@ -0,0 +1,8 @@
+//Exercício 8: Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado
+let vinteCinco = [];
+
+for (let i=1; i<26; i++) {
+  vinteCinco.push(i);
+}
+
+console.log(vinteCinco);
