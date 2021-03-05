a0d4c99ac7635e67a32d1290853d6e4f47c4df7f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-05 18:47:31 -0300

    Adicionando dias do mes de dezembro no calendario

--- FUNDAMENTOS/BLOCO_5/DIA_3/script.js
+++ FUNDAMENTOS/BLOCO_5/DIA_3/script.js
@@ -14,3 +14,28 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+//1- inserir dias no calendário
+function insertDaysInCalendar() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById('days');
+  for (const index in dezDaysList) {
+    let daysListItem = document.createElement('li');
+    daysListItem.className = 'day';
+    daysListItem.innerHTML = dezDaysList[index];
+    if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
+      daysListItem.className = 'day holiday';
+    }
+
+    if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
+      daysListItem.className = 'day friday';
+    }
+
+    if (dezDaysList[index] === 25) {
+      daysListItem.className = 'day holiday friday';
+    }
+    
+    daysList.appendChild(daysListItem);
+  }
+}
+
+insertDaysInCalendar();
