7a137b042f757be8ac0727eecc4ace13c1ed61e9
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-09 13:42:36 -0300

    Implementaçao das funçoes que trocam os seguintes estilos: background-color, color, font-size e line-height. As configuraçoes que o usuario informar serao salvas no localStorage para uma nova visita.

--- FUNDAMENTOS/BLOCO_5/DIA_4/script.js
+++ FUNDAMENTOS/BLOCO_5/DIA_4/script.js
@@ -6,23 +6,50 @@
   fontFamily: ['Roboto', 'Roboto Slab', 'Open Sans', 'Lato', 'Slabo']
 };
 const selectArray = document.querySelectorAll('select'); */
+window.onload = function () {
+  document.body.style.backgroundColor = localStorage.getItem('cor-de-fundo');
+  document.body.style.color = localStorage.getItem('color');
+  for (let index = 0; index < paragraphs.length; index += 1) {
+    paragraphs[index].style.lineHeight = `${localStorage.getItem('line-height')}px`;
+  }
+  for (let index = 0; index < textPage.length; index += 1) {
+    textPage[index].style.fontSize = `${localStorage.getItem('font-size')}`;
+  }
+}
+
 const inputBackgroundColor = document.getElementById('input-backgroundColor');
 const inputColor = document.getElementById('input-color');
-//const inputLineHeight = document.getElementById('input-lineHeight');
+const inputLineHeight = document.getElementById('input-lineHeight');
+const inputFontSize = document.getElementById('input-fontSize');
+const paragraphs = document.querySelectorAll('p');
+const textPage = document.querySelector('article').children;
 
 function changeBackgroundColor() {
   document.body.style.backgroundColor = inputBackgroundColor.value;
+  localStorage.setItem('cor-de-fundo', inputBackgroundColor.value);
 }
-document.addEventListener('click', changeBackgroundColor);
+inputBackgroundColor.addEventListener('change', changeBackgroundColor);
 
 function changeColorFont() {
   document.body.style.color = inputColor.value;
+  localStorage.setItem('color', inputColor.value);
 }
-document.addEventListener('click', changeColorFont);
+inputColor.addEventListener('change', changeColorFont);
+
+function changeFontSize() {
+  for (let index = 0; index < textPage.length; index += 1) {
+    textPage[index].style.fontSize = `${inputFontSize.value}`;
+  }
+  localStorage.setItem('font-size', inputFontSize.value);
+}
+inputFontSize.addEventListener('change', changeFontSize);
 
 function changeLineHeight() {
-  document.querySelector('section').children.style.lineHeight = inputLineHeight.value;
+  for (let index = 0; index < paragraphs.length; index += 1) {
+    paragraphs[index].style.lineHeight = `${inputLineHeight.value}px`;
+  }
+  localStorage.setItem('line-height', inputLineHeight.value);
 }
-document.addEventListener('click', changeLineHeight);
+inputLineHeight.addEventListener('change', changeLineHeight);
 
 
