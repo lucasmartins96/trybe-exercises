987def4c03ca32c349094c40a7a025ac0dc14a22
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-23 18:53:57 -0300

    Parte 1 - Exercicio 2 completo

--- FUNDAMENTOS/BLOCO_7/DIA_1/parte1_exercicios.js
+++ FUNDAMENTOS/BLOCO_7/DIA_1/parte1_exercicios.js
@@ -14,3 +14,30 @@ function testingScope(escopo) {
   }
 }
 testingScope(true);
+
+/**
+2 - faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+ */
+const oddsAndEvens = () => {
+  const arr = [13, 3, 4, 10, 7, 2];
+  for (let nextIndex = 1; nextIndex < arr.length; nextIndex++) {
+    for (let currentIndex = 0; currentIndex < nextIndex; currentIndex++) {
+      if (arr[nextIndex] < arr[currentIndex]) {
+        let aux = arr[nextIndex];
+        arr[nextIndex] = arr[currentIndex];
+        arr[currentIndex] = aux;
+      }
+    }
+  }
+  return `Os números ${arr} se encontram ordenados de forma crescente!`;
+}
+
+/*Bônus */
+const oddsAndEvensSorted = () => {
+  const arr = [13, 3, 4, 10, 7, 2];
+  arr.sort((a, b) => a - b);
+  return `Os números ${arr} se encontram ordenados de forma crescente!`;
+}
+console.log(oddsAndEvensSorted());
