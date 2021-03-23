6cb572f2d8ef2dd8b29f70dba3489ee8494df304
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-23 18:13:53 -0300

    Removendo linha do comentario do exercicio 1

--- FUNDAMENTOS/BLOCO_7/DIA_1/parte1_exercicios.js
+++ FUNDAMENTOS/BLOCO_7/DIA_1/parte1_exercicios.js
@@ -2,7 +2,6 @@
 1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 Modifique a estrutura da função para que ela seja uma arrow function .
 Modifique as concatenações para template literals .
-Copie o código abaixo.
  */
 function testingScope(escopo) {
   if (escopo === true) {
