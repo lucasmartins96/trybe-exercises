fd98212763b8edddf7918214c262daf95da1d464
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-25 19:19:21 -0300

    Requisito 3 completo

--- FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-myfizzbuzz.js
+++ FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-myfizzbuzz.js
@@ -14,3 +14,6 @@ assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
 
 //2 - Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
 assert.strictEqual(myFizzBuzz(18), 'fizz');
+
+//3 - Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(20), 'buzz');
