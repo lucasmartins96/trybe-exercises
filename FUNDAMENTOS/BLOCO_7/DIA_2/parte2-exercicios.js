74fe63cd50df9b3c221da2fa7a698d060719d052
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-24 22:53:11 -0300

    Parte 2 - Exercicio 5 completo

--- FUNDAMENTOS/BLOCO_7/DIA_2/parte2-exercicios.js
+++ FUNDAMENTOS/BLOCO_7/DIA_2/parte2-exercicios.js
@@ -44,3 +44,28 @@ showObjLenght(lesson3);
 */
 const showObjValues = obj => Object.values(obj);
 showObjValues(lesson2);
+
+/*
+5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 
+Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . 
+Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
+console.log(allLessons);
