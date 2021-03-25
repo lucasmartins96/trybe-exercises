6c00406d1bfbfa2a1b2776ecc04cbd51e7ef9838
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-25 19:00:00 -0300

    Requisito 1 completo

---
+++ FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-myremovewithoutcopy.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+// implemente seus testes aqui
+//1 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
