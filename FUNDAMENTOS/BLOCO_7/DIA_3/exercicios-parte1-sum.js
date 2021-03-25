573c7dcc0aa165bfde57ca3480b6482ffb407fbe
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-25 17:50:38 -0300

    Refatorando os requisitos 1 e 2

--- FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
+++ FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
@@ -10,12 +10,10 @@ function sum(a, b) {
 
 // implemente seus testes aqui
 //1 - Teste se o retorno de sum(4, 5) é 9
-const expected9 = sum(5, 4);
-assert.strictEqual(expected9, 9, 'Resultado diferente de 9');
+assert.strictEqual(sum(5, 4), 9, 'Resultado diferente de 9');
 
 //2 - Teste se o retorno de sum(0, 0) é 0
-const expected0 = sum(0, 0);
-assert.strictEqual(expected0, 0, 'Resultado diferente de 0');
+assert.strictEqual(sum(0, 0), 0, 'Resultado diferente de 0');
 
 //3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 assert.throws(() => {sum(4, '5');});
