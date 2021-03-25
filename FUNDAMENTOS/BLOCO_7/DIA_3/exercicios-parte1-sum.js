4523e5eeaccacb709397ecce443433bf430e1ef2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-25 17:47:21 -0300

    Requisito 4 completo

--- FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
+++ FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
@@ -19,3 +19,6 @@ assert.strictEqual(expected0, 0, 'Resultado diferente de 0');
 
 //3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 assert.throws(() => {sum(4, '5');});
+
+//4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throws(() => {sum(4, '5');}, /^Error: parameters must be numbers$/);
