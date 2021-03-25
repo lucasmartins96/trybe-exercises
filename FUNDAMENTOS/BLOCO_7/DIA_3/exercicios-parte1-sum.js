78737f1f1f938aa469ada6eacec8c5f3c581ea69
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-25 15:56:44 -0300

    Requisito 2 completo e mudança no nome da variavel do requisito 1

--- FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
+++ FUNDAMENTOS/BLOCO_7/DIA_3/exercicios-parte1-sum.js
@@ -10,5 +10,9 @@ function sum(a, b) {
 
 // implemente seus testes aqui
 //1 - Teste se o retorno de sum(4, 5) é 9
-const expected = sum(5, 4);
-assert.strictEqual(expected, 9, 'Resultado diferente de 9');
+const expected9 = sum(5, 4);
+assert.strictEqual(expected9, 9, 'Resultado diferente de 9');
+
+//2 - Teste se o retorno de sum(0, 0) é 0
+const expected0 = sum(0, 0);
+assert.strictEqual(expected0, 0, 'Resultado diferente de 0');
