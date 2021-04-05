852c55ae42f492f0d6b05e4e229412c2a56a85af
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-05 11:07:43 -0300

    Adicionando o gabarito do exercicio 7 da parte 2

--- FUNDAMENTOS/BLOCO_7/DIA_2/parte2-exercicios.js
+++ FUNDAMENTOS/BLOCO_7/DIA_2/parte2-exercicios.js
@@ -49,23 +49,6 @@ showObjValues(lesson2);
 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 
 Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . 
 Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
-{
-  lesson1:
-   { materia: 'Matemática',
-     numeroEstudantes: 20,
-     professor: 'Maria Clara',
-     turno: 'manhã' },
-  lesson2:
-   { materia: 'História',
-     numeroEstudantes: 20,
-     professor: 'Carlos',
-     turno: 'noite' },
-  lesson3:
-   { materia: 'Matemática',
-     numeroEstudantes: 10,
-     professor: 'Maria Clara',
-     turno: 'noite' }
-};
 */
 const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
 
@@ -87,11 +70,8 @@ showStudentsAmount(allLessons);
 console.log(getValueByNumber(lesson1, 0));
 // Output: 'Matématica'
 */
-const getValueByNumber = (obj, index) => {
-  const objValues = Object.values(obj);
-  return (objValues[index]) ? objValues[index] : 'not found';
-}
-getValueByNumber(lesson1, 0);
+const getValueByNumber = (obj, number) => Object.values(obj)[number];
+console.log(getValueByNumber(lesson1, 0));
 
 /*
 8 - Crie uma função que verifique se o par (chave / valor) existe na função. 
