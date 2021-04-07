53006cff890e2fc6ef567f9e0f3e86354b20902c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-07 15:14:09 -0300

    Fixaçao spread

---
+++ FUNDAMENTOS/BLOCO_8/DIA_5/fixacao.js
@@ -0,0 +1,16 @@
+/*
+Fixação Spread
+
+Para fixar e praticar, vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens , faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread .
+*/
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Banana', 'Maçã', 'Pêra'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Leite condensado', 'leite em pó', 'chocolate'];
+
+const fruitSalad = (fruit, additional) => {
+  console.log(...fruit, ...additional);
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
