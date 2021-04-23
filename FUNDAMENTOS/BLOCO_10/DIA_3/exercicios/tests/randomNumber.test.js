fa806bfab43c858598430f0caa1a540d83faf6c0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-23 10:26:18 -0300

    Adicionando a resoluçao do exercicio 2 e 3

--- FUNDAMENTOS/BLOCO_10/DIA_3/exercicios/tests/randomNumber.test.js
+++ FUNDAMENTOS/BLOCO_10/DIA_3/exercicios/tests/randomNumber.test.js
@@ -15,4 +15,24 @@ describe('randomNumber tests', () => {
   it('Verifica se foi chamada 1 vez', () => {
     expect(randomNumber).toHaveBeenCalledTimes(1);
   });
+
+  it('Verifica se a função foi implementada uma vez para dividir', () => {
+    randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+    expect(randomNumber(8, 2)).toBe(4);
+    expect(randomNumber(8, 2)).not.toBe(4);
+  });
+
+  it('Verifica se a função foi implementada com a multiplicação de 3 números', () => {
+    randomNumber = jest.fn((a, b, c) => a * b * c);
+    expect(randomNumber(8, 2, 4)).not.toBe(undefined);
+    expect(randomNumber(8, 2, 4)).toBe(64);
+  });
+
+  it('Verifica se a função foi implementada retornando o dobro do parâmetro', () => {
+    randomNumber.mockReset();
+    randomNumber = jest.fn((number) => number * 2);
+    expect(randomNumber(8)).not.toBe(undefined);
+    expect(randomNumber(8)).not.toBe(NaN);
+    expect(randomNumber(8)).toBe(16);
+  });
 });
