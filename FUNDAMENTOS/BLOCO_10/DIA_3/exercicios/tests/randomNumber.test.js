8c67e950355dbe02b45bb82a9e1efa67399b1f0f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-23 10:38:19 -0300

    Adicionando o gabarito da exercicio 1

--- FUNDAMENTOS/BLOCO_10/DIA_3/exercicios/tests/randomNumber.test.js
+++ FUNDAMENTOS/BLOCO_10/DIA_3/exercicios/tests/randomNumber.test.js
@@ -1,18 +1,10 @@
 let { randomNumber } = require('../randomNumber');
 
 describe('randomNumber tests', () => {
-  it('Verifica se a função é chamada', () => {
-    randomNumber = jest.fn();
-    randomNumber();
-    expect(randomNumber).toHaveBeenCalled();
-  });
-
-  it('Verifica se o retorno padrão é 10', () => {
+  it('Testando se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
     randomNumber = jest.fn().mockReturnValue(10);
     expect(randomNumber()).toBe(10);
-  });
-
-  it('Verifica se foi chamada 1 vez', () => {
+    expect(randomNumber).toHaveBeenCalled();
     expect(randomNumber).toHaveBeenCalledTimes(1);
   });
 
