627ada76ecf456e5a5795174b5c6867535d766a8
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-21 18:10:38 -0300

    react input tests

--- FRONT-END/BLOCO_15/dia-2/src/App.js
+++ FRONT-END/BLOCO_15/dia-2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MyForm from './form/MyForm';
 import Joke from './joke/Joke';
 
 function App() {
   return (
     <div className="App">
       <Joke />
+      <MyForm />
     </div>
   );
 }
