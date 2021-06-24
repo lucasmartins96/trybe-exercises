0fe178bed46263f11cf1e2ab9d21d9593d677623
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-24 00:13:41 -0300

    adicionando o exercício 2

--- FRONT-END/BLOCO_18/dia-3/src/App.js
+++ FRONT-END/BLOCO_18/dia-3/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CustomTimer from './exercise_one/CustomTimer';
+import TodoApp from './exercise_two/TodoApp';
 
 const App = () => {
   return (
     <div>
       <CustomTimer />
+      <TodoApp />
     </div>
   );
 };
---
+++ FRONT-END/BLOCO_18/dia-3/src/exercise_two/TodoApp.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import useArray from './useArray';
+
+function TodoApp() {
+  const [newInput, setNewInput] = useState('');
+  const { addTodos, todos } = useArray();
+
+  const handleInput = ({ target }) => {
+    setNewInput(target.value);
+  };
+
+  const handleClick = () => {
+    addTodos(newInput);
+    setNewInput('');
+  };
+
+  return (
+    <div>
+      <label htmlFor="task-input">
+        Add a task:
+        <input
+          id="task-input"
+          placeholder="task"
+          name="newItem"
+          value={newInput}
+          onChange={handleInput}
+        />
+      </label>
+      <button type="button" onClick={() => handleClick()}>
+        Add
+      </button>
+      <TodoList tasks={todos} />
+    </div>
+  );
+}
+
+export default TodoApp;
