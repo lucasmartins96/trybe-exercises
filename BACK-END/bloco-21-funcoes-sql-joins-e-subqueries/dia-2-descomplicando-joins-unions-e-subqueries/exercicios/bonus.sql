f5cfb65fd84db427bb9c595e8c9f99e5437f5ee3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-27 12:04:04 -0300

    resolução do exercício 12 (bonus)

--- BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios/bonus.sql
+++ BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios/bonus.sql
@@ -26,5 +26,20 @@ FROM
 WHERE
     mov1.director = mov2.director;
 
--- Exercício 12: Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem o título dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
+-- Exercício 12: Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN, que retornem o título dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
+-- INNER JOIN
+SELECT
+    m.title
+FROM
+    Pixar.BoxOffice bo 
+        INNER JOIN
+    Pixar.Movies m ON m.id = bo.movie_id 
+WHERE 
+    (bo.domestic_sales > 500000000 OR bo.international_sales > 500000000)
+        AND
+    m.length_minutes > 110;
 
+-- SUBQUERY
+SELECT (SELECT title FROM Pixar.Movies WHERE length_minutes > 110 AND id = bo.movie_id) filme FROM Pixar.BoxOffice bo WHERE (bo.domestic_sales > 500000000 OR bo.international_sales > 500000000);
+
+SELECT m.title FROM Pixar.Movies m WHERE m.id IN (SELECT bo.movie_id FROM Pixar.BoxOffice bo WHERE bo.domestic_sales > 500000000 OR bo.international_sales > 500000000) AND m.length_minutes > 110;
