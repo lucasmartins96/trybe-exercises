484c256a655f7030017ccc230dc8498b96dce593
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-27 10:11:00 -0300

    resolução do exercício 12

--- BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios/exercicios-do-dia.sql
+++ BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios/exercicios-do-dia.sql
@@ -87,7 +87,7 @@ WHERE
 SELECT * FROM hr.employees;
 
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
-
+SELECT * FROM hr.employees e WHERE LENGTH(FIRST_NAME) >= 8;
 
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano).
 -- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
