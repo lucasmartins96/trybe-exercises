9d5b46dd54aae49348b15e2d70b780a98df40ec0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-27 12:17:22 -0300

    adicionando resoluções do gabarito

--- BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios/exercicios-do-dia.sql
+++ BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios/exercicios-do-dia.sql
@@ -9,7 +9,14 @@ FROM
     Pixar.Movies mov ON box.movie_id = mov.id;
 
 -- Exercício 2: Utilizando o INNER JOIN, faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais (internationa_sales) do que vendas nacionais (domestic_sales).
-
+SELECT 
+    m.title, b.domestic_sales, b.international_sales
+FROM
+    Pixar.Movies m
+        INNER JOIN
+    Pixar.BoxOffice b ON b.movie_id = m.id
+WHERE
+    b.international_sales > b.domestic_sales;
 
 -- Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua avaliação (rating) em ordem decrescente.
 SELECT 
@@ -22,10 +29,33 @@ FROM
     ORDER BY mov.title DESC, box.rating DESC;
 
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
--- SELECT * FROM Pixar.Theater t LEFT JOIN Pixar.Movies m ON t.id = m.theater_id;
+SELECT 
+    t.name,
+    t.location,
+    m.title,
+    m.director,
+    m.year,
+    m.length_minutes
+FROM
+    Pixar.Theater t
+        LEFT JOIN
+    Pixar.Movies m ON t.id = m.theater_id
+ORDER BY t.name;
 
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
--- SELECT mov.id, mov.title, mov.director, mov.`year`, mov.length_minutes, mov.theater_id FROM Pixar.Movies mov RIGHT JOIN Pixar.Theater the ON the.id = mov.theater_id;
+SELECT 
+    t.id,
+    t.name,
+    t.location,
+    m.title,
+    m.director,
+    m.year,
+    m.length_minutes
+FROM
+    Pixar.Theater t
+        RIGHT JOIN
+    Pixar.Movies m ON t.id = m.theater_id
+ORDER BY t.name;
 
 
 -- Exercício 6: Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN, que retornem os títulos dos filmes que possuem avaliação maior que 7.5.
