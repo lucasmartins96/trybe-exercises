e883f5de40db0f48b9f8bdd6ff397b7bd085e413
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-27 10:17:49 -0300

    resolução do exercício 15

--- BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios/exercicios-do-dia.sql
+++ BACK-END/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios/exercicios-do-dia.sql
@@ -96,6 +96,8 @@ SELECT EMPLOYEE_ID, FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees;
 SELECT EMPLOYEE_ID, FIRST_NAME, DAY(HIRE_DATE) FROM hr.employees;
 
 -- 15. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e mês no qual foi contratado (exiba somente o mês).
+SELECT EMPLOYEE_ID, FIRST_NAME, MONTH(HIRE_DATE) FROM hr.employees;
+
 -- 16. Escreva uma query que exiba os nomes dos funcionários em letra maiúscula.
 -- 17: Escreva uma query que exiba o sobrenome e a data de contratação de todos os funcionário contratados em julho de 1987.
 -- 18: Escreva uma query que exiba as seguintes informações de cada funcionário: nome, sobrenome, tempo que trabalha na empresa (em dias).
