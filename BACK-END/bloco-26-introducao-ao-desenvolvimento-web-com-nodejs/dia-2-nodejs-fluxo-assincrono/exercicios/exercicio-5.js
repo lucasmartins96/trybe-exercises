adf0d0fd9545693b212e7a5edbaf97d24a7f884a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-28 15:48:09 -0300

    resolução do exercicio 5

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-5.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-5.js
@@ -1,33 +1,27 @@
-/*
-Crie uma função que lê e escreve vários arquivos ao mesmo tempo.
-  
-  - Utilize o Promise.all para manipular vários arquivos ao mesmo tempo.
-  
-  - Dado o seguinte array de strings: ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'] 
-    Faça com que sua função crie um arquivo contendo cada string, sendo o nome de cada arquivo igual a file<index + 1>.txt . 
-    Por exemplo, para a string "Finalmente", o nome do arquivo é file1.txt .
-  
-  - Programe sua função para que ela faça a leitura de todos os arquivos criados no item anterior, armazene essa informação e escreva em um arquivo chamado fileAll.txt .
-
-O conteúdo do arquivo fileAll.txt deverá ser Finalmente estou usando Promise.all !!!
-*/
-
 const fs = require('fs').promises;
 
-const arrStr = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
+async function arrayToFile() {
+  const strings = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
 
-const createNewFilesPromises = arrStr.map((str, index) => {
-  return fs.writeFile(`file${index + 1}.txt`, str);
-});
+  const createFilePromises = strings.map((string, index) =>
+    fs.writeFile(`./file${index + 1}.txt`, string)
+  );
 
-// console.log(createNewFilesPromises);
+  await Promise.all(createFilePromises);
 
-Promise.all(createNewFilesPromises)
-  .then((files) => {
-    console.log(files);
-    /* files.forEach(async (file) => {
-      const newContent = await fs.readFile(file, 'utf8');
-      console.log(file);
-      fs.writeFile('fileAll.txt', newContent + " ");
-    }); */
-  });
+  const fileNames = [
+    'file1.txt',
+    'file2.txt',
+    'file3.txt',
+    'file4.txt',
+    'file5.txt',
+  ];
+
+  const fileContents = await Promise.all(
+    fileNames.map((fileName) => fs.readFile(fileName, 'utf-8'))
+  );
+
+  const newFileContent = fileContents.join(' ');
+
+  await fs.writeFile('./fileAll.txt', newFileContent);
+}
