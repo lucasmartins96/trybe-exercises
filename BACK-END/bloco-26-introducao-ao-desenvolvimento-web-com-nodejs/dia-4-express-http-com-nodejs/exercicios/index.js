0d338621a6dcb9cfe192fc580dde00b7a0e1a5ce
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-28 12:09:35 -0300

    resolução do exercício 2

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/index.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/index.js
@@ -8,6 +8,11 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!`});
+});
+
 app.listen(3001, () => {
   console.log('App listening on port 3001!');
 });
