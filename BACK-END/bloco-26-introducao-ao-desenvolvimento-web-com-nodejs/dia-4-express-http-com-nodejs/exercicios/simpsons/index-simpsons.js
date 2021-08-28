8e2068b1a86c1d03b34c1701a8fdc44897bab519
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-28 13:15:26 -0300

    resolução do exercício 6 e tratamento de erro

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/simpsons/index-simpsons.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios/simpsons/index-simpsons.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getSimpsonsData } = require('./file-management');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/simpsons', async (req, res) => {
+  try {
+    const simpsonsData = await getSimpsonsData();
+    res.status(200).json(simpsonsData);    
+  } catch (error) {
+    res.status(500).end();
+  }
+});
 
 app.listen(3002, () => {
   console.log('App listening on port 3002!');
