cd19f2fe83e6718bbc2ed85748f0739ab5387287
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-30 22:03:51 -0300

    separando as validações

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios/user/index.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios/user/index.js
@@ -5,26 +5,54 @@ const PORT = 3001;
 const app = express();
 app.use(bodyParser.json());
 
-function validateUserData(req, res, next) {
-  const { username, email, password } = req.body;
-  
-  const isValidUsernameLength = username.length > 3;
-  const isValidEmail = /\w+@\w+.com/g.test(email);
-  const isValidPasswordLength = 
-    (password.toString().length >= 4 && password.toString().length <= 8) && typeof password === 'number';
+function validateUsernameLength(req, res, next) {
+  const { username } = req.body;
 
-  if (!isValidUsernameLength || !isValidEmail || !isValidPasswordLength) {
-    console.log(isValidUsernameLength, isValidEmail, isValidPasswordLength);
-    return res.status(400).json({ "message": "email or password is incorrect" });
+  if (username.length <= 3) {    
+    return res.status(400).json({ "message": "username is incorrect" });
   }
-  
+
   next();
 }
 
-app.post('/user/register', validateUserData, (req, res) => {
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+
+  const emailRegex = new RegExp(/\w+@\w+.com/, 'g');
+
+  if (!emailRegex.test(email)) {    
+    return res.status(400).json({ "message": "email is incorrect" });
+  }
+
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+
+  const isValidPasswordLength = 
+    password.toString().length < 4 || password.toString().length > 8 || typeof password !== 'number';
+
+  if (isValidPasswordLength) {
+    return res.status(400).json({ "message": "password is incorrect" });
+  }
+
+  next();
+}
+
+app.post(
+  '/user/register',
+  validateUsernameLength,
+  validateEmail,
+  validatePassword, 
+  (req, res) => {
   const { username, email, password } = req.body;
   return res.status(201).json({ "message": "user created" });
 });
 
+app.post('/user/login', (req, res) => {
+  const { email, password } = req.body;
+  
+});
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`)});
