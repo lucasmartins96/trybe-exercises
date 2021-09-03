8728a49fa6b8ae5580325a965b3a2ebe1605ceb2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-02 22:16:10 -0300

    adicionando as validações

---
+++ BACK-END/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/user-mongo/middlewares/validations.js
@@ -0,0 +1,67 @@
+const BAD_REQUEST_STATUS = 400;
+
+function validateFirstName(req, res, next) {
+  const { firstName } = req.body;
+  if (!firstName)
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "O campo 'firstName' é obrigatório",
+    });
+
+  next();
+}
+
+function validateLastName(req, res, next) {
+  const { lastName } = req.body;
+  if (!lastName)
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "O campo 'lastName' é obrigatório",
+    });
+
+  next();
+}
+
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  if (!email)
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "O campo 'email' é obrigatório",
+    });
+
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "O campo 'password' é obrigatório",
+    });
+  }
+
+  if (password.length < 5) {
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "O campo 'password' deve ter pelo menos 6 caracteres",
+    });
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(BAD_REQUEST_STATUS).json({
+      error: true,
+      message: "'password'inválido",
+    });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateFirstName,
+  validateLastName,
+  validateEmail,
+  validatePassword,
+};
