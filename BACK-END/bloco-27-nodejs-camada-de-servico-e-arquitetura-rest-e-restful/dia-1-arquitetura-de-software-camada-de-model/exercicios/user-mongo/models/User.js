780544f5f7485abf7e53c8f879ec02f691288dd1
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-02 23:52:12 -0300

    adicionando a função findById do model User

--- BACK-END/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/user-mongo/models/User.js
+++ BACK-END/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/user-mongo/models/User.js
@@ -1,9 +1,12 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
+
+const collectionUsers = 'users';
 
 const create = async (firstName, lastName, email, password) => {
   const db = await connection();
   const result = await db
-    .collection('users')
+    .collection(collectionUsers)
     .insertOne({ firstName, lastName, email, password });
   return {
     id: result.insertedId,
@@ -16,7 +19,7 @@ const create = async (firstName, lastName, email, password) => {
 
 const getAll = async () => {
   const db = await connection();
-  const users = await db.collection('users').find().toArray();
+  const users = await db.collection(collectionUsers).find().toArray();
   const usersWithId = users.map(({ _id, firstName, lastName, email, password }) => ({
     id: _id,
     firstName,
@@ -27,7 +30,17 @@ const getAll = async () => {
   return usersWithId;
 };
 
+const findById = async (id) => {
+  const db = await connection();
+  const userData = await db.collection(collectionUsers).findOne(new ObjectId(id));
+
+  if (!userData) return null;
+
+  return userData;
+};
+
 module.exports = {
   create,
   getAll,
+  findById,
 };
