c2a4ef1e2b8d9f3e4511887b362f421c1c14d74e
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-02 22:46:39 -0300

    adicionando a função que retorna getAll

--- BACK-END/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/user-mongo/models/User.js
+++ BACK-END/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/user-mongo/models/User.js
@@ -2,16 +2,32 @@ const connection = require('./connection');
 
 const create = async (firstName, lastName, email, password) => {
   const db = await connection();
-  const result = await db.collection('users').insertOne({ firstName, lastName, email, password });
+  const result = await db
+    .collection('users')
+    .insertOne({ firstName, lastName, email, password });
   return {
     id: result.insertedId,
     firstName,
     lastName,
     email,
-    password
+    password,
   };
+};
+
+const getAll = async () => {
+  const db = await connection();
+  const users = await db.collection('users').find().toArray();
+  const usersWithId = users.map(({ _id, firstName, lastName, email, password }) => ({
+    id: _id,
+    firstName,
+    lastName,
+    email,
+    password,
+  }));
+  return usersWithId;
 };
 
 module.exports = {
   create,
+  getAll,
 };
